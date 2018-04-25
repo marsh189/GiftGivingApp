efd7f72b451b4545a682c46aa1f76936f775f503
marsh189 <marsh189@mail.chapman.edu>
2018-04-24 17:10:00 -0700

    Working nav bar with different fragments

--- GiftGiving/app/src/main/java/com/hci/marsh189/giftgiving/MainActivity.java
+++ GiftGiving/app/src/main/java/com/hci/marsh189/giftgiving/MainActivity.java
@@ -4,60 +4,48 @@ import android.support.annotation.NonNull;
 import android.support.design.widget.BottomNavigationView;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    private ActionBar toolbar;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        BottomNavigationView bottomNavigationView = (BottomNavigationView)
+                findViewById(R.id.navigation);
 
-        toolbar = getSupportActionBar();
-        BottomNavigationView bottonNav = (BottomNavigationView) findViewById(R.id.navigationView);
-        bottonNav.setOnNavigationItemSelectedListener(NavItemSelectedListener);
-    }
+        bottomNavigationView.setOnNavigationItemSelectedListener
+                (new BottomNavigationView.OnNavigationItemSelectedListener() {
+                    @Override
+                    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                        Fragment selectedFragment = null;
+                        switch (item.getItemId()) {
+                            case R.id.friendsBtn:
+                                selectedFragment = FriendsFragment.newInstance();
+                                break;
+                            case R.id.groupsBtn:
+                                selectedFragment = GroupsFragment.newInstance();
+                                break;
+                            case R.id.profileBtn:
+                                selectedFragment = ProfileFragment.newInstance();
+                                break;
+                        }
+                        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+                        transaction.replace(R.id.frame_layout, selectedFragment);
+                        transaction.commit();
+                        return true;
+                    }
+                });
 
-    private BottomNavigationView.OnNavigationItemSelectedListener NavItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item)
-        {
-            Fragment fragment;
-            switch (item.getItemId())
-            {
-                case R.id.navigation_friends:
-                    toolbar.setTitle("Friends");
-                    fragment = new FriendsFragment();
-                    loadFragment(fragment);
-                    return true;
-                case R.id.navigation_groups:
-                    toolbar.setTitle("Groups");
-                    fragment = new GroupsFragment();
-                    loadFragment(fragment);
-                    return true;
-                case R.id.navigation_profile:
-                    toolbar.setTitle("Profile");
-                    fragment = new FriendsFragment();
-                    loadFragment(fragment);
-                    return true;
-            }
-            return false;
-        }
-    };
-
-    private void loadFragment(Fragment fragment) {
-        // load fragment
+        //Manually displaying the first fragment - one time only
         FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
-        transaction.replace(R.id.container, fragment);
-        transaction.addToBackStack(null);
+        transaction.replace(R.id.frame_layout, ProfileFragment.newInstance());
         transaction.commit();
-    }
 
+        //Used to select an item programmatically
+        //bottomNavigationView.getMenu().getItem(2).setChecked(true);
+    }
 }
